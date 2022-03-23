d8cb62ecf3975ac9b11adb37abf5b931c0eb21d4
JakubMizera <kubamiz53@gmail.com>
2022-03-23 09:23:26 +0100

    sending sum

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.send('hello' + sum);
+    req.query.sum;
 });
 app.listen(3000);
+var num1 = 10;
+var num2 = 5;
+var sum = num1 + num2;
+console.log(num1 * num2);
--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
 const express = require('express')  
 const app = express()  
 app.get('/', function (req, res) {  
-  res.send('Hello World')  
+  res.send('hello' + sum)
+  req.query.sum
 })  
 app.listen(3000)  
+
+const num1 = 10;
+const num2 = 5;
+
+const sum = num1 + num2;
+
+console.log(num1 * num2);   
