7a5f018e4480cdf0d40ce6773235cf2fb37ec9f2
JakubMizera <kubamiz53@gmail.com>
2022-03-23 12:26:05 +0100

    GET, PUT, DELETE functionality

--- lab2/index.ts
+++ lab2/index.ts
@@ -2,7 +2,7 @@
 // const app = express();
 
 import express from 'express';
-import e, {Request, Response} from "express";
+import e, { Request, Response } from "express";
 const app = express();
 
 app.use(express.json());
@@ -22,19 +22,19 @@ class Note {
     //     this.tags = tags || null;
     //     this.id = Date.now();
     // }
-    constructor(note: Note){
-            this.title = note.title;
-            this.content = note.content;
-            this.createDate = note.createDate || new Date();
-            this.tags = note.tags || undefined;
-            this.id = Date.now();
-        }
+    constructor(note: Note) {
+        this.title = note.title;
+        this.content = note.content;
+        this.createDate = note.createDate || new Date();
+        this.tags = note.tags || undefined;
+        this.id = Date.now();
+    }
 
 }
 
 let notes: Note[] = [];
 
-notes.push(new Note({title:"test", content:"testcontent"}))
+notes.push(new Note({ title: "test", content: "testcontent" }))
 
 
 app.get('/notes', (req: Request, res: Response) => {
@@ -46,14 +46,41 @@ app.post('/note', (req: Request, res: Response) => {
     //console.log(req.body.note)
     let note = new Note(req.body.note)
     notes.push(note)
-    
+
     res
         .sendStatus(200)
         .send('Notatka została dodana')
 })
 
 app.get('/note/:id', (req: Request, res: Response) => {
-    let findNote = notes.find(note => note.id === Number(req.params.id))    
-    
+    let findNote = notes.find(note => note.id === Number(req.params.id))
+    if (findNote) {
+        res
+            .sendStatus(200)
+            .send(findNote)
+    } else {
+        res.sendStatus(404).send("Nie ma takiej notatki")
+    }
 })
+
+app.put('/note/:id', (req: Request, res: Response) => {
+    let findNote = notes.find(note => note.id === Number(req.params.id))
+    if (findNote) {
+        findNote = new Note({ ...findNote, ...req.body.note })
+        res.sendStatus(204);
+    } else {
+        res.sendStatus(404).send("Nie można edytować notatki")
+    }
+})
+
+app.delete('/note/:id', (req: Request, res: Response) => {
+    let findNote = notes.findIndex(note => note.id === Number(req.params.id))
+    if (findNote) {
+        notes.splice(findNote, 1)
+        res.sendStatus(204).send("Usunięto notatkę")
+    } else {
+        res.sendStatus(400).send("Nie można usunąć notatki")
+    }
+})
+
 app.listen(3000);
