085952157f71bcf2efdcae79759f655a802415c8
JakubMizera <kubamiz53@gmail.com>
2022-05-31 11:04:07 +0200

    project

--- projekt/routes/VendorRoute.ts
+++
@@ -1,9 +0,0 @@
-import express, {Request, Response, NextFunction} from "express";
-
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.json({message: 'hello vendor'})
-})
-
-export {router as VendorRoute};
